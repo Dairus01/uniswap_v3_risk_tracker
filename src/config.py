d4c0fd4afece1a6ede733871f90e73c019a39b66
ihebabididotio <iheb.abidi@yields.digital>
2025-07-30 17:47:12 +0200

    config_code_formatting

--- src/config.py
+++ src/config.py
@@ -4,18 +4,20 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Blockchain connections
-INFURA_URL = os.getenv('INFURA_URL')
+INFURA_URL = os.getenv("INFURA_URL")
 
-#SUBGRAPH CONNECTIONS   
-subgraph_api_key = os.getenv('subgraph_api_key')
-subgraph_id = os.getenv('subgraph_id')
-SUBGRAPH_URL = f"https://gateway.thegraph.com/api/{subgraph_api_key}/subgraphs/id/{subgraph_id}"
+# SUBGRAPH CONNECTIONS
+subgraph_api_key = os.getenv("subgraph_api_key")
+subgraph_id = os.getenv("subgraph_id")
+SUBGRAPH_URL = (
+    f"https://gateway.thegraph.com/api/{subgraph_api_key}/subgraphs/id/{subgraph_id}"
+)
 
 # Target pools (USDC/ETH, WBTC/ETH, LINK/ETH)
 POOLS = {
     "USDC_ETH": "0x8ad599c3a0ff1de082011efddc58f1908eb6e6d8",
     "WBTC_ETH": "0xcbcdf9626bc03e24f779434178a73a0b4bad62ed",
-    "LINK_ETH": "0xa6cc3c2531fdaa6ae1a3ca84c2855806728693e8"
+    "LINK_ETH": "0xa6cc3c2531fdaa6ae1a3ca84c2855806728693e8",
 }
 
 # Chainlink ETH/USD price feed
@@ -28,7 +30,13 @@ PREDEFINED_ABIS = {
             "name": "latestAnswer",
             "outputs": [{"internalType": "int256", "name": "", "type": "int256"}],
             "stateMutability": "view",
-            "type": "function"
+            "type": "function",
         }
     ]
 }
+
+
+# database connections
+REDIS_HOST = os.getenv("REDIS_HOST")
+REDIS_PORT = os.getenv("REDIS_PORT")
+REDIS_DB = os.getenv("REDIS_DB")
